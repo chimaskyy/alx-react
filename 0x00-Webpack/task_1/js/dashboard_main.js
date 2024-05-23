76978da41893e7a6067771fc77146ab088656da9
chimaskyy <chiamakahj@gmail.com>
2024-05-23 14:55:38 +0100

    fixes

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -4,15 +4,13 @@ import _ from "lodash";
 let count = 0;
 
 function updateCounter() {
-    count += 1;
-    $('#count').text(`${count} clicks on the button`)
+  count += 1;
+  $("#count").text(`${count} clicks on the button`);
 }
 
-$(function () {
-  $("body").append("<p>Holberton Dashboard</p>");
-  $("body").append("<p>Dashboard data for the students</p>");
-  $("body").append("<button id='click'>Click here to get started</button>");
-  $("body").append("<p><p id='count'></p></p>");
-  $("body").append("<p>Copyright - Holberton School</p>");
-  $('#click').on('click', _.debounce(updateCounter, 500));
-});
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button id='click'>Click here to get started</button>");
+$("body").append("<p><p id='count'></p></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
+$("#click").on("click", _.debounce(updateCounter, 500));
