28d639f5da5d433f7dca37f3c89e1a5c25825c47
chimaskyy <chiamakahj@gmail.com>
2024-06-03 17:17:47 +0100

    aphrodite

---
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const rowStyle = {
+  backgroundColor: "#f5f5f5ab",
+};
+
+const headStyle = {
+  backgroundColor: "#deb5b545",
+};
+function CourseListRow({
+  isHeader = false,
+  textFirstCell,
+  textSecondCell = null,
+}) {
+  
+  return (
+    <tr style={rowStyle}>
+      {isHeader ? (
+        textSecondCell === null ? (
+          <th style={headStyle} colSpan={2}>{textFirstCell}</th>
+        ) : (
+          <>
+            <th>{textFirstCell}</th>
+            <th>{textSecondCell}</th>
+          </>
+        )
+      ) : (
+        <>
+          <td>{textFirstCell}</td>
+          <td>{textSecondCell}</td>
+        </>
+      )}
+    </tr>
+  );
+}
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null,
+};
+
+export default CourseListRow;
---
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notification/Notifications.js
@@ -0,0 +1,111 @@
+import React from "react";
+import PropTypes from "prop-types";
+import NotificationItem from "./NotificationItem";
+import NotificationItemShape from "./NotificationItemShape";
+import closeIcon from "../close-icon.png";
+import { StyleSheet, css } from "aphrodite";
+
+class Notifications extends React.Component {
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  // ensures that the component will only re-render when new notifications are added.
+  shouldComponentUpdate(nextProps) {
+    return nextProps.length > this.props.listNotifications.length;
+  }
+
+  render() {
+    const { displayDrawer, listNotifications } = this.props;
+
+    return (
+      <>
+        <div className={css(styles.menuItem)}>Your notifications</div>
+        {displayDrawer && (
+          <div className={css(styles.notification)}>
+            <button
+              style={{
+                color: "#3a3a3a",
+                fontWeight: "bold",
+                background: "none",
+                border: "none",
+                fontSize: "15px",
+                position: "absolute",
+                right: "2px",
+                top: "2px",
+                cursor: "pointer",
+              }}
+              aria-label="Close"
+              onClick={() => console.log("Close button has been clicked")}
+            >
+              <img src={closeIcon} alt="closeIcon" />
+            </button>
+            <p>Here is the list of notifications</p>
+            <ul>
+              {listNotifications.length === 0 ? (
+                <NotificationItem
+                  type="default"
+                  value="No new notification for now"
+                />
+              ) : (
+                listNotifications.map((notification) => (
+                  <NotificationItem
+                    key={notification.id}
+                    type={notification.type}
+                    value={notification.value}
+                    html={notification.html}
+                    markAsRead={this.markAsRead}
+                    id={notification.id}
+                  />
+                ))
+              )}
+            </ul>
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  notification: {
+    padding: "-1em",
+    marginBottom: "20px",
+    border: "2px dashed red",
+    position: "absolute",
+    top: "2.5em",
+    right: 0,
+    width: "25%",
+  },
+  menuItem: {
+    textAlign: 'right'
+  },
+  '[data-notification-type="default"]': {
+    color: "blue",
+  },
+
+  "[data-urgent]": {
+    color: "red",
+  },
+
+  '[data-notification-type="urgent"]': {
+    color: "red",
+  },
+});
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+};
+
+export default Notifications;
