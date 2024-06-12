0feae5c44eb95eeac155c6ce5fe4ac4b8c863f6f
chimaskyy <chiamakahj@gmail.com>
2024-06-12 16:00:06 +0100

    nested element

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,17 @@
+import { fromJS } from "immutable";
+// const {fromJS} = require('immutable')
+
+export default function accessImmutableObject(object, array) {
+    const immutableObj = fromJS(object);
+    const valPath = immutableObj.getIn(array);
+    return valPath
+}
+
+// const example = accessImmutableObject({
+//      name: {
+//           first: "Guillaume",
+//           last: "Salva"
+//      }
+// }, ['name', 'first'])
+
+// console.log(example)
