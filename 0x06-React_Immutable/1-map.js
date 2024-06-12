91a72cd7c98632b2e2dda5aa4d4063459d92c625
chimaskyy <chiamakahj@gmail.com>
2024-06-12 15:27:28 +0100

    fromjs to map

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,6 +1,5 @@
-const { Map } = require("immutable");
+import { Map } from "immutable";
 
-export default function getImmutableObject(object) {
-  return Map(object);
-}
+const getImmutableObject = (object) => Map(object);
 
+export default getImmutableObject;
